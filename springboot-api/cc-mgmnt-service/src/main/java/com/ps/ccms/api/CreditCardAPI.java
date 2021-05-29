c2939817e283a0b4040780d697372a1184a91c8e
sibendra30 <sibendra30singh@gmail.com>
2021-05-29 20:29:22 +0100

    Adding API spec yaml file

--- springboot-api/cc-mgmnt-service/src/main/java/com/ps/ccms/api/CreditCardAPI.java
+++ springboot-api/cc-mgmnt-service/src/main/java/com/ps/ccms/api/CreditCardAPI.java
@@ -1,6 +1,7 @@
 package com.ps.ccms.api;
 
 import com.ps.ccms.models.Card;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
 import java.util.List;
 
 @RequestMapping("/cards")
+@Produces("application/json")
 public interface CreditCardAPI {
 
     @GetMapping
     ResponseEntity<List<Card>> getCards();
 
     @PostMapping
+    @Consumes("application/json")
     ResponseEntity<Card> addNewCard(@Validated @RequestBody Card requestBody);
 }
